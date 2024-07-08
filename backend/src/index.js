fb47060110268fc7105789a213f99c553f13d481
Ildi Dvorani <dvoranii@ildidev.com>
2024-07-08 14:27:08 -0400

    added code to decode base64 .env string value back into json format

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,6 +12,12 @@ const port = process.env.PORT || 5000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const base64String = process.env.FIRESTORE_SERVICE_ACCOUNT;
+const jsonString = Buffer.from(base64String, "base64").toString("utf-8");
+const jsonObject = JSON.parse(jsonString);
+
+console.log(jsonObject);
+
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from the backend!" });
 });
